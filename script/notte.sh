57d1d4af9b3d408e1c4a9ae4f39d78e3d897e43c
Matteo Migliarini <matteo.miglairini@uniroma1.it>
2025-12-21 09:32:34 +0000

    Add utility functions and update requirements for vLLM integration

---
+++ script/notte.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+source script/utils.sh
+
+models=(
+    "Qwen/Qwen3-30B-A3B-Instruct-2507"
+    "Qwen/Qwen3-30B-A3B-Instruct-2507-FP8"
+    "Qwen/Qwen3-30B-A3B-Thinking-2507"
+    "Qwen/Qwen3-8B"
+    "Qwen/Qwen3-32B"
+    # Qwen 235B later
+    # mistral 70B later
+    "mistralai/Mistral-Nemo-Instruct-2407" # 12B
+   "microsoft/phi-4"
+   "google/gemma-3-12b-it"
+    # "meta-llama/Llama-3.1-8B-Instruct"
+  #  "meta-llama/llama-3.3-70b-instruct"
+   # "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
+   "allenai/Olmo-3.1-32B-Instruct"
+    "nvidia/Llama-3.3-70B-Instruct-FP8"
+    "openai/gpt-oss-20b"
+    "openai/gpt-oss-120b"
+    # "mistralai/Ministral-3-14B-Reasoning-2512" # Need vllm 0.12
+    # "mistralai/Ministral-3-14B-Instruct-2512"
+ #   "allenai/Olmo-3-32B-Think"
+    # "nvidia/NVIDIA-Nemotron-Nano-12B-v2"
+#    "nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16"
+    "mistralai/Devstral-Small-2-24B-Instruct-2512"
+    "meta-llama/Llama-4-Scout-17B-16E-Instruct"
+    # "Qwen/Qwen3-235B-A22B-Instruct-2507-FP8"
+    # "Qwen/Qwen3-235B-A22B-Thinking-2507-FP8"
+)
+
+for model in "${models[@]}"; do
+    kill_vllm
+    bash script/kappa.sh $model > vllm.log 2>&1 &
+    wait_vllm
+
+    seeds=(
+        10 20 30 40 50
+    )
+
+    for seed in "${seeds[@]}"; do
+        bash script/eval.sh --model vllm/$model --seed $seed --max-connections 16
+    done
+done
+
+kill_vllm
