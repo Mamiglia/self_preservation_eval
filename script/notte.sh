79e7c1f77a42dd2dc5769f994696a2ecba8b1c9b
Matteo Migliarini <matteo.miglairini@uniroma1.it>
2025-12-28 17:25:48 +0000

    Update model list

--- script/notte.sh
+++ script/notte.sh
@@ -3,29 +3,32 @@ source script/utils.sh
 
 models=(
     "Qwen/Qwen3-30B-A3B-Instruct-2507"
-    "Qwen/Qwen3-30B-A3B-Instruct-2507-FP8"
+    #"Qwen/Qwen3-30B-A3B-Instruct-2507-FP8"
     "Qwen/Qwen3-30B-A3B-Thinking-2507"
-    "Qwen/Qwen3-8B"
-    "Qwen/Qwen3-32B"
+    # "Qwen/Qwen3-30B-A3B-Thinking-2507-FP8"
+    #"Qwen/Qwen3-8B"
+    #"Qwen/Qwen3-32B"
     # Qwen 235B later
     # mistral 70B later
-    "mistralai/Mistral-Nemo-Instruct-2407" # 12B
-   "microsoft/phi-4"
-   "google/gemma-3-12b-it"
+    #"mistralai/Mistral-Nemo-Instruct-2407" # 12B
+    #"microsoft/phi-4"
+    #"microsoft/Phi-4-reasoning"
+    #"google/gemma-3-12b-it"
     # "meta-llama/Llama-3.1-8B-Instruct"
   #  "meta-llama/llama-3.3-70b-instruct"
    # "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
-   "allenai/Olmo-3.1-32B-Instruct"
-    "nvidia/Llama-3.3-70B-Instruct-FP8"
-    "openai/gpt-oss-20b"
-    "openai/gpt-oss-120b"
+    #"allenai/Olmo-3.1-32B-Instruct"
+    #"nvidia/Llama-3.3-70B-Instruct-FP8"
+    #"openai/gpt-oss-20b"
+    #"openai/gpt-oss-120b"
     # "mistralai/Ministral-3-14B-Reasoning-2512" # Need vllm 0.12
     # "mistralai/Ministral-3-14B-Instruct-2512"
  #   "allenai/Olmo-3-32B-Think"
     # "nvidia/NVIDIA-Nemotron-Nano-12B-v2"
-#    "nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16"
-    "mistralai/Devstral-Small-2-24B-Instruct-2512"
-    "meta-llama/Llama-4-Scout-17B-16E-Instruct"
+    #"nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16"
+    # "google/gemma-3-27b-it"
+    #"mistralai/Devstral-Small-2-24B-Instruct-2512"
+    #"meta-llama/Llama-4-Scout-17B-16E-Instruct"
     # "Qwen/Qwen3-235B-A22B-Instruct-2507-FP8"
     # "Qwen/Qwen3-235B-A22B-Thinking-2507-FP8"
 )
